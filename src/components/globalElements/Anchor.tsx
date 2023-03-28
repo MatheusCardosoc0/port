7247abb158eb5db63c5fb5da6d90901c227cc82f
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2023-03-28 13:02:09 -0300

    adicionando detalhes

--- src/components/globalElements/Anchor.tsx
+++ src/components/globalElements/Anchor.tsx
@@ -10,7 +10,7 @@ const Anchor = ({children, link, color}: AnchorProps) => {
   return (
     <a href={link}
       target={"_blank"}
-      className={`mx-auto flex flex-col items-center text-4xl rounded-full ${color? color: 'bg-common'} p-1 w-[100px] h-[100px] justify-center relative overflow-hidden`}>
+      className={`mx-auto flex flex-col items-center text-4xl rounded-full ${color? color: 'bg-common'} p-1 w-[100px] h-[100px] justify-center relative hover:bg-sky-500 overflow-hidden`}>
       <span className='RIPPLE-EFFECT-TWO' />
       {children}
     </a>
