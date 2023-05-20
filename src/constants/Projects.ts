52d74cc6ddd80327d5e355077b8a7f1260b5dbd7
MatheusCardosoc0 <matheuscardosoc905@gmail.com>
2023-05-20 19:57:05 -0300

    atualizando o portifolio

--- src/constants/Projects.ts
+++ src/constants/Projects.ts
@@ -11,5 +11,11 @@ export const Projects: ProjectProps[] = [
     githubLink: 'https://github.com/MatheusCardosoc0/filmcache_remake',
     viewLink: 'https://filmcache-remake.vercel.app/',
     image: '/assets/cine_see.png'
+  },
+  {
+    description: 'Pokedex listando todos os pokemons na pokeApi, com sistema de busca e um sistema de filtros por tipo, peso e altura.',
+    githubLink: 'https://github.com/MatheusCardosoc0/pokemon_project',
+    image: '/assets/pokedex.png',
+    viewLink: 'https://pokemon-project-liart.vercel.app/'
   }
 ]
